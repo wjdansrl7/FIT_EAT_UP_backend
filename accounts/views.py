0ac4c615356a661491a80243c126b60f26bef850
moongi <c880910@naver.com>
2022-08-05 11:48:08 +0900

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from .serializers import SignupSerializer
+
+
+class SignupView(CreateAPIView):
+    model = get_user_model
+    serializer_class = SignupSerializer
+    permission_classes = [
+        AllowAny,
+    ]
+
+
