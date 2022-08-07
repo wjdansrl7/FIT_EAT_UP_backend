8ce1a2ccf1574a7599b3b7d403644070ad0935e3
moongi <c880910@naver.com>
2022-08-08 00:02:54 +0900

    token url 생성

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignupView.as_view(), name='login',)
+    path('signup/', views.SignupView.as_view(), name='login'), # 회원가입을 위한 url : localhost:8000/accounts/signup
+    path('token/', obtain_jwt_token), # token 발급 : localhost:8000/accounts/tokenv
+    path('token/refresh/', refresh_jwt_token),
+    path('token/verify/', verify_jwt_token),
 ]
