7033999493f5694ce958bb035b71d5d00f54ed2d
moongi <c880910@naver.com>
2022-08-14 20:13:02 +0900

    version 0.2.1

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class User(AbstractUser):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
-    nickname = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=30, unique=True)
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers, status
+from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.response import Response
-from rest_framework.validators import UniqueValidator
+# from rest_framework.response import Response
+# from rest_framework.validators import UniqueValidator
+
 
 User = get_user_model()
 
@@ -20,19 +21,19 @@ class SignupSerializer(serializers.ModelSerializer):
         fields = ['pk', 'username', 'password', 'nickname']
 
 
-class NicknameUniqueCheckSerializer(serializers.ModelSerializer):
-    nickname = serializers.CharField(required=True, min_length=1,
-                                     max_length=30, validators=[UniqueValidator(queryset=User.objects.all())])
-
-    def post(self, request, format=None):
-        serializer = self.get_serializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            return Response(data={'detail': ['You can use this nickname']}, status=status.HTTP_200_OK)
-        else:
-            detail = dict()
-            detail['detail'] = serializer.errors['nickname']
-            return Response(data=detail, status=status.HTTP_400_BAD_REQUEST)
-
-    class Meta:
-        model = User
-        fields = ['nickname']
+# class NicknameUniqueCheckSerializer(serializers.ModelSerializer):
+#     nickname = serializers.CharField(required=True, min_length=1,
+#                                      max_length=30, validators=[UniqueValidator(queryset=User.objects.all())])
+#
+#     def post(self, request, format=None):
+#         serializer = self.get_serializer(data=request.data, context={'request': request})
+#         if serializer.is_valid():
+#             return Response(data={'detail': ['You can use this nickname']}, status=status.HTTP_200_OK)
+#         else:
+#             detail = dict()
+#             detail['detail'] = serializer.errors['nickname']
+#             return Response(data=detail, status=status.HTTP_400_BAD_REQUEST)
+#
+#     class Meta:
+#         model = User
+#         fields = ['nickname']
