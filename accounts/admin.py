f705b487e4efd49ad342310c9e73936e6ac3ed7e
moongi <c880910@naver.com>
2022-08-25 21:05:57 +0900

    version 0.3.0

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,8 +3,16 @@ from django.contrib import admin
 from .models import User
 
 
+# 장고 admin 페이지에 User 생성
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     pass
+
+
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+#     pass
+
+
 
 
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 from . import views
 
 urlpatterns = [
-    # path('uniquecheck/nickname', views.NicknameUniqueCheck.as_view(), name='uniquecheck_nickname'), #닉네임 중복여부검사를 위한 url : localhost:8000/accounts/uniquecheck/nickname
-    path('signup/', views.SignupView.as_view(), name='login'), # 회원가입을 위한 url : localhost:8000/accounts/signup
-    path('token/', obtain_jwt_token), # token 발급 : localhost:8000/accounts/tokenv
+    path('signup/', views.SignupView.as_view(), name='login'),  # 회원가입을 위한 url : localhost:8000/accounts/signup
+    path('token/', obtain_jwt_token),  # token 발급 : localhost:8000/accounts/token
     path('token/refresh/', refresh_jwt_token),
     path('token/verify/', verify_jwt_token),
+    path('user/list', views.ProfileListAPIView.as_view(), name='profile_user_list'),  # localhost:8000/accounts/user/list 유저 리스트 조회
+    path('user/', views.ProfileAPIView.as_view(), name='profile_user'),  # 유저 정보 조회: pk, username, nickname / localhost:8000/accounts/user
+    path('user/<int:pk>/update/', views.ProfileUpdateAPIView.as_view(), name='profile_user_update'),  # pk에 따른 유저 정보 수정: localhost:8000/accounts/pk/update
+
 ]
